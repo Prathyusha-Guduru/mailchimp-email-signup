41e0774e4b030a2b1e15ef26a3f5005b6cf90922
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-07-27 13:33:22 +0530

    installed dependencies

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Helloooo");
+});
+app.listen(3000, () => {
+  console.log("server up and running");
+});
