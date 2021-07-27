56e015bb5f91340ee3191a727ee680c1a494f5cf
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-07-27 13:49:48 +0530

    Updated forms

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const request = require("request");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Helloooo");
+  res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", (req, res) => {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+});
+
 app.listen(3000, () => {
   console.log("server up and running");
 });
